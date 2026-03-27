db342632c52c2f62b16f01610ceeff216c4574f8
Vismay Churiwala <vismaychuriwala@users.noreply.github.com>
2026-03-27 15:44:45 -0400

    added scalar CPU EPA Python wrapper

--- scalar/examples/python_ctypes/src/pyopengjk/__init__.py
+++ scalar/examples/python_ctypes/src/pyopengjk/__init__.py
@@ -4,6 +4,8 @@ openGJK is a fast and robust C implementation of the
 Gilbert-Johnson-Keerthi (GJK) algorithm.
 """
 
-from .opengjk import compute_minimum_distance, Point3, Simplex, DistanceResult, USE_32BITS
+from .opengjk import (compute_minimum_distance, compute_collision_information,
+                      Point3, Simplex, DistanceResult, CollisionResult, USE_32BITS)
 
-__all__ = ["compute_minimum_distance", "Point3", "Simplex", "DistanceResult", "USE_32BITS"]
+__all__ = ["compute_minimum_distance", "compute_collision_information",
+           "Point3", "Simplex", "DistanceResult", "CollisionResult", "USE_32BITS"]
